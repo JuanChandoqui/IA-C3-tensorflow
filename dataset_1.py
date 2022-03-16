296369de0fb18a9b913dcd89e9ffa486e6cff7d1
JuanChandoqui <193242@ids.upchiapas.edu.mx>
2022-03-15 22:41:32 -0600

    names of files changed

--- dataset_1.py
+++ dataset_1.py
@@ -7,47 +7,47 @@ import tensorflow as tf
 def leerTxt(archivo: str):
     file = f'./Datasets/{archivo}.txt'
 
-    data_X = np.loadtxt(file, delimiter='\t', dtype=np.float32, skiprows=1, usecols=[0,1,2])
-    data_Y = np.loadtxt(file, delimiter='\t', dtype=np.float64, skiprows=1, usecols=[4])
+    data = np.loadtxt(file, delimiter='\t', dtype=np.float32, skiprows=1, usecols=[0])
+    data_2 = np.loadtxt(file, delimiter='\t', dtype=np.float32, skiprows=1, usecols=[1])
 
-    array_X_aux = np.split(data_X,1199);
     array_X = []
     array_Y = []
 
-    for value in data_Y:
+    for value in data:
+        array_X.append(value)
+
+    for value in data_2:
         array_Y.append(value)
 
-    for i in range(len(array_X_aux)):
-        array_X.append(array_X_aux[i][0])
-
+    print(f'ARRAY Y: {array_Y}')
     return array_X, array_Y
 
 
 if __name__ == "__main__":
     #LEER TXT
-    column_1 , column_2 = leerTxt('dataset02')
+    column_1 , column_2 = leerTxt('dataset01')
 
     # cargamos las 4 combinaciones de las compuertas XOR
     x = np.array(column_1, "float32")
 
     # y estos son los resultados que se obtienen, en el mismo orden
-    y = np.array(column_2, "float64")
+    y = np.array(column_2, "float32")
 
     #tasa de aprendizaje
-    n = 0.1
+    n = 0.1 
     
     # Create the 'Perceptron' using the Keras API
     model = tf.keras.models.Sequential()
     #inicia con pesos aleatorios con kernel_initializer
-    model.add(tf.keras.layers.Dense(1, input_dim=3, activation='linear', kernel_initializer='glorot_uniform', bias_initializer='Ones'))
+    model.add(tf.keras.layers.Dense(1, input_dim=1, activation='linear', kernel_initializer='glorot_uniform', bias_initializer='zeros'))
     
     #se utiliza adam ya que permite ajustar los pesos y sesgos, para que pueda mejorar durante el entrenamiento
     #se coloca la tasa de aprendizaje en Adam
     model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(n)) 
-    
+
     # Train the perceptron using stochastic gradient descent
     # with a validation split of 20%
-    historial = model.fit(x, y, epochs=250, batch_size=25, verbose=False)
+    historial = model.fit(x, y, epochs=150, batch_size=25, verbose=False)
     result = model.predict(x)
     print(f'PESOS: {model.get_weights()}')
     
--- dataset_2.py
+++ dataset_2.py
@@ -7,39 +7,39 @@ import tensorflow as tf
 def leerTxt(archivo: str):
     file = f'./Datasets/{archivo}.txt'
 
-    data = np.loadtxt(file, delimiter='\t', dtype=np.float32, skiprows=1, usecols=[0])
-    data_2 = np.loadtxt(file, delimiter='\t', dtype=np.float32, skiprows=1, usecols=[1])
+    data_X = np.loadtxt(file, delimiter='\t', dtype=np.float32, skiprows=1, usecols=[0,1,2])
+    data_Y = np.loadtxt(file, delimiter='\t', dtype=np.float64, skiprows=1, usecols=[4])
 
+    array_X_aux = np.split(data_X,1199);
     array_X = []
     array_Y = []
 
-    for value in data:
-        array_X.append(value)
-
-    for value in data_2:
+    for value in data_Y:
         array_Y.append(value)
 
-    print(f'ARRAY Y: {array_Y}')
+    for i in range(len(array_X_aux)):
+        array_X.append(array_X_aux[i][0])
+
     return array_X, array_Y
 
 
 if __name__ == "__main__":
     #LEER TXT
-    column_1 , column_2 = leerTxt('dataset01')
+    column_1 , column_2 = leerTxt('dataset02')
 
-    # cargamos las 4 combinaciones de las compuertas XOR
+    #se carga las entradas del dataset
     x = np.array(column_1, "float32")
 
-    # y estos son los resultados que se obtienen, en el mismo orden
-    y = np.array(column_2, "float32")
+    #se carga las salidas del dataset
+    y = np.array(column_2, "float64")
 
     #tasa de aprendizaje
-    n = 0.1 
+    n = 0.1
     
     # Create the 'Perceptron' using the Keras API
     model = tf.keras.models.Sequential()
     #inicia con pesos aleatorios con kernel_initializer
-    model.add(tf.keras.layers.Dense(1, input_dim=1, activation='linear', kernel_initializer='glorot_uniform', bias_initializer='zeros'))
+    model.add(tf.keras.layers.Dense(1, input_dim=3, activation='linear', kernel_initializer='glorot_uniform', bias_initializer='Ones'))
     
     #se utiliza adam ya que permite ajustar los pesos y sesgos, para que pueda mejorar durante el entrenamiento
     #se coloca la tasa de aprendizaje en Adam
@@ -47,13 +47,13 @@ if __name__ == "__main__":
     
     # Train the perceptron using stochastic gradient descent
     # with a validation split of 20%
-    historial = model.fit(x, y, epochs=150, batch_size=25, verbose=False)
+    historial = model.fit(x, y, epochs=250, batch_size=25, verbose=False)
     result = model.predict(x)
     print(f'PESOS: {model.get_weights()}')
     
-    plt.plot(historial.history['loss'], label=f'n={n}')
-    print(f'RESULT: {result}')
-    plt.legend()
-    plt.xlabel("iteraciones")
-    plt.ylabel("errores")
-    plt.show()
+    # plt.plot(historial.history['loss'], label=f'n={n}')
+    # print(f'RESULT: {result}')
+    # plt.legend()
+    # plt.xlabel("iteraciones")
+    # plt.ylabel("errores")
+    # plt.show()
